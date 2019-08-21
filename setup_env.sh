c9472d8684ea7f7a0acf34accbc569c4d2688bf6
Gunjae Koo (@franck) <gunjae.koo@gmail.com>
2019-08-21 16:09:12 +0900

    changed Xilinx license server

--- setup_env.sh
+++ setup_env.sh
@@ -2,5 +2,6 @@
 
 # environment for Xilinx Vivado
 . /opt/Xilinx/Vivado/2014.4/settings64.sh
-export XILINXD_LICENSE_FILE=1713@ics-lic.usc.edu
-export LM_LICENSE_FILE=1715@lic-modelsim.usc.edu
+#export XILINXD_LICENSE_FILE=1713@ics-lic.usc.edu
+#export LM_LICENSE_FILE=1715@lic-modelsim.usc.edu
+export XILINXD_LICENSE_FILE=2100@203.249.80.222
