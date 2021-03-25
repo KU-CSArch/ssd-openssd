be858b460976a6388048919a533369093af1241a
Gunjae Koo (@albeniz) <gunjae.koo@gmail.com>
2021-03-25 12:46:21 +0900

    updated the license server

--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,5 @@
 . /opt/Xilinx/Vivado/2014.4/settings64.sh
 #export XILINXD_LICENSE_FILE=1713@ics-lic.usc.edu
 #export LM_LICENSE_FILE=1715@lic-modelsim.usc.edu
-export XILINXD_LICENSE_FILE=2100@203.249.80.222
+#export XILINXD_LICENSE_FILE=2100@203.249.80.222
+export XILINXD_LICENSE_FILE=2100@163.152.162.76
